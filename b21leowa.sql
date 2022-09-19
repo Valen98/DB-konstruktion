fe7f491e22813c3e6ae3dc6b18b35e22ceaec99c
Valen98 <leo.wahlandtcs@gmail.com>
2022-09-19 19:42:45 +0200

    Database is almost complete with triggers and procedures

--- b21leowa.sql
+++ b21leowa.sql
@@ -1,7 +1,5 @@
-DROP DATABASE B21leowa;
+DROP DATABASE b21leowa;
 CREATE DATABASE b21leowa;
-DROP USER 'barn'@'localhost';
-DROP USER 'admin'@'localhost';
 FLUSH PRIVILEGES;
 USE b21leowa;
 
@@ -12,22 +10,56 @@ CREATE TABLE barn(
 	PNR VARCHAR(14) NOT NULL,
     namn VARCHAR(255) NOT NULL,
     födelseår DATE NOT NULL,
+    snällhetsSkala INTEGER,
     PRIMARY KEY (PNR, namn)
 )ENGINE=INNODB;
 
--- CREATE INDEX barnPNR ON barn(PNR);
+CREATE INDEX barnFödelseår ON barn(födelseår);
+
+ CREATE TABLE barnRelation(
+    PNR1 VARCHAR(14) NOT NULL,
+    namn1 VARCHAR(255) NOT NULL,
+    PNR2 VARCHAR(14) NOT NULL,
+    namn2 VARCHAR(255) NOT NULL,
+    typAvRelation VARCHAR(255) NOT NULL,
+	FOREIGN KEY (PNR1, namn1) REFERENCES barn(PNR, namn),
+    FOREIGN KEY (PNR2, namn2) REFERENCES barn(PNR, namn),
+    PRIMARY KEY (PNR1, namn1, PNR2, namn2)
+)ENGINE=INNODB;
+
+CREATE TABLE barnKod (
+	ID INTEGER NOT NULL,
+    snällhet VARCHAR(255),
+    PRIMARY KEY(ID)
+)ENGINE=INNODB;
+
+
+CREATE TABLE barnLog (
+	ID INTEGER NOT NULL AUTO_INCREMENT,
+    OPERATION CHAR(3),
+	username VARCHAR(32),
+    PNR VARCHAR(14),
+    namn VARCHAR(255),
+    tid DATETIME,
+    tabell VARCHAR(255),
+    PRIMARY KEY(ID)
+)ENGINE=INNODB;
 
 -- INSERT DEFAULT CHILD FOR TESTING THE DB
 INSERT INTO barn(PNR, namn, födelseår) VALUES ("20090909-0909", "Anders Andersson", "2009-09-09");
 
+# Sysslor är antal sysslor som barnet har tilldelats och hur av dom sysslor hen har utfört. Därefter beräknas hjälpsamhet genom sysslor/utfördaSysslor
 -- TABLE FOR BARN INHERITANCE
 CREATE TABLE snälltBarn(
 	PNR VARCHAR(14) NOT NULL,
     namn VARCHAR(255) NOT NULL,
-	hjälpsamhet INTEGER,
+    sysslor INTEGER,
+	utfördaSysslor INTEGER,
     FOREIGN KEY (PNR, namn) REFERENCES barn(PNR, namn),
     PRIMARY KEY(PNR, namn)
 )ENGINE=INNODB;
+
+INSERT INTO snälltBarn(PNR, namn, sysslor, utfördaSysslor) VALUES ("20090909-0909", "Anders Andersson", 12, 10);
 
 -- TABLE FOR BARN INHERITANCE
 CREATE TABLE mindreSnälltBarn(
@@ -39,6 +71,7 @@ CREATE TABLE mindreSnälltBarn(
     PRIMARY KEY(PNR, namn)
 )ENGINE=INNODB;
 
+
 -- Horizontell split from child and childText
 CREATE TABLE barnBeskrivning (
     beskrivning TEXT,
@@ -46,6 +79,15 @@ CREATE TABLE barnBeskrivning (
     namn VARCHAR(255) NOT NULL,
 	FOREIGN KEY (PNR, namn) REFERENCES barn(PNR, namn),
 	PRIMARY KEY(PNR, namn)
+)ENGINE=INNODB;
+
+CREATE TABLE barnBeskrivningLOG(
+	ID INTEGER NOT NULL AUTO_INCREMENT,
+    OPERATION CHAR(3),
+    PNR VARCHAR(14) NOT NULL,
+    namn VARCHAR(255) NOT NULL,
+    tid DATETIME,
+    PRIMARY KEY(ID)
 )ENGINE=INNODB;
 
 CREATE TABLE inspelning(
@@ -95,7 +137,7 @@ CREATE TABLE önseklistaBeskrivning (
 )ENGINE=INNODB;
 
 
-xCREATE TABLE önskelistaViewVärde (
+CREATE TABLE önskelistaViewVärde (
      årtal DATE NOT NULL,
      PNR CHAR(14) NOT NULL,
      pris FLOAT
@@ -111,12 +153,16 @@ SELECT * FROM barnBeskrivning;
 
 -- Users
 
-CREATE USER 'barn'@'localhost' IDENTIFIED BY 'barn';
+CREATE USER IF NOT EXISTS 'barn'@'localhost' IDENTIFIED BY 'barn';
 
-CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin';
+CREATE USER IF NOT EXISTS 'tomten'@'localhost' IDENTIFIED BY 'tomten';
+
+GRANT SELECT, DELETE, UPDATE ON * TO 'tomten'@'localhost';
+
+SHOW GRANTS FOR 'admin'@'localhost';
 
 SELECT * FROM önskelista, barn WHERE önskelista.PNR = barn.PNR;
-
 
  -- Views 
 #A view where the child for his/hers own wishlist
@@ -128,37 +174,98 @@ CREATE VIEW barnetsInspelning AS SELECT inspelning.tid, inspelning.beskrivning, 
 
 SELECT * FROM barnetsInspelning;
 
+# Skapar en vy för att räkna ut hur mycket hjälpsamhet ett snälltbarn har. 0 är inga gjorda sysslor och 100 är alla sysslor gjorda.
+CREATE VIEW hjälpsamhet AS SELECT utfördaSysslor/sysslor*100 AS hjälpsamhet, PNR, namn FROM snälltBarn;
+
+SELECT * FROM hjälpsamhet;
+
 /*GIVES THE CHILDREN ACCESS TO SELECT, DELETE and UPDATE to barnetsÖnskelista and barnetsInspelning */
 GRANT SELECT, DELETE, UPDATE ON barnetsÖnskelista TO 'barn'@'localhost';
 GRANT SELECT, DELETE, UPDATE ON barnetsInspelning TO 'barn'@'localhost';
 
-SELECT PNR as bPNR FROM barn;
-
--- PUT ALL PROCEDURE INSIDE THE DELIMITER
+-- PUT ALL PROCEDURE AND TRIGGERS INSIDE THE DELIMITER
 DELIMITER //
-CREATE PROCEDURE insertChild(iPNR VARCHAR(14), iNamn VARCHAR(255), iFödelseår DATE)  
-BEGIN 
-    INSERT INTO barn(PNR, namn, födelseår) VALUES (iPNR, iNamn, iFödelseår);
-END;
-
-
 CREATE PROCEDURE uppdateraBarnBeskrivning(iPNR VARCHAR(14), iNamn VARCHAR(255), iBeskrivning TEXT)
 BEGIN 
-DECLARE uPNR VARCHAR(14);
-SELECT PNR INTO uPNR FROM barnBeskrivning;
-SELECT UPNR;
-    IF (uPNR=iPNR) THEN
-        UPDATE barnBeskrivning SET beskrivning = iBeskrivning WHERE PNR = uPNR;
+    IF (SELECT PNR FROM barnBeskrivning WHERE PNR = iPNR AND namn = iNamn) THEN
+        UPDATE barnBeskrivning SET beskrivning = iBeskrivning WHERE PNR = iPNR AND namn = iNamn;
     ELSE 
         INSERT INTO barnBeskrivning(PNR, namn, beskrivning) VALUES (iPNR, iNamn, iBeskrivning);
-        SELECT 'Lägger in ett nytt meddelande' FROM barnBeskrivning;
-    END IF;
+END IF;
+END; 
 
+#PROCEDURE Som flyttar ifall ett barn går från snäll till mindre snällt.
+CREATE PROCEDURE tillMindreSnälltBarn(iPNR VARCHAR(14), iNamn VARCHAR(255), iNivå INTEGER, iLeveransNummer CHAR(10)) 
+BEGIN
+	IF (SELECT PNR FROM snälltBarn WHERE PNR = iPNR AND namn = iNamn) THEN 
+		DELETE FROM snälltBarn WHERE PNR = iPNR AND namn = iNamn;
+		INSERT INTO mindreSnälltBarn(PNR, namn, nivå, leveransNummer) VALUES (iPNR, iNamn, iNivå , iLeveransNummer);
+	ELSE 
+		IF(SELECT PNR FROM mindreSnälltBarn WHERE PNR != iPNR AND namn != iNamn) THEN
+			INSERT INTO mindreSnälltBarn(PNR, namn, nivå, leveransNummer) VALUES (iPNR, iNamn, iNivå , iLeveransNummer);
+		END IF;
+	END IF;
+END;
+
+#PROCEDURE Som flyttar ifall ett barn går från mindre snäll till snällt. Ifall 
+CREATE PROCEDURE tillSnälltBarn(iPNR VARCHAR(14), iNamn VARCHAR(255)) 
+BEGIN
+	#Kollar ifall barnet är med i mindreSnällt och ifall barnet är där tas barnet bort från mindre snällt tabellen.
+	IF (SELECT PNR FROM mindreSnälltBarn WHERE PNR = iPNR AND namn = iNamn) THEN 
+		DELETE FROM mindreSnälltBarn WHERE PNR = iPNR AND namn = iNamn;
+		INSERT INTO snälltBarn(PNR, namn) VALUES (iPNR, iNamn);
+	ELSE 
+		IF(SELECT PNR FROM snälltBarn WHERE PNR != iPNR AND namn != iNamn) THEN
+			INSERT INTO snälltBarn(PNR, namn) VALUES (iPNR, iNamn);
+		END IF;
+	END IF;
+END;
+    
+CREATE TRIGGER tillSnälltBarnTrigger AFTER INSERT ON snälltBarn
+ FOR EACH ROW BEGIN
+	INSERT INTO barnLog(OPERATION, username, PNR, namn, tid, tabell) VALUES ("INS", USER(), new.PNR, new.namn, NOW(), "snälltBarn");
+END;
+
+CREATE TRIGGER tillMindreSnälltBarnTrigger AFTER INSERT ON mindreSnälltBarn
+ FOR EACH ROW BEGIN
+ INSERT INTO barnLog(OPERATION, username, PNR, namn, tid, tabell) VALUES ("INS", USER(), new.PNR, new.namn, NOW(), "mindreSnälltBarn");
+ END;
+ 
+ CREATE TRIGGER barnBeskrvningTriggerINS AFTER INSERT ON barnBeskrivning
+ FOR EACH ROW BEGIN 
+	  INSERT INTO barnBeskrivningLOG(OPERATION, PNR, namn, tid) VALUES ("INS", new.PNR, new.Namn, NOW()); 
+END;
+
+CREATE TRIGGER barnBeskrivningTriggerUDP AFTER UPDATE ON barnBeskrivning
+FOR EACH ROW BEGIN
+	INSERT INTO barnBeskrivningLOG(OPERATION, PNR, namn, tid) VALUES ("UPD", new.PNR, new.Namn, NOW()); 
 END//
 DELIMITER ;
 
 
-CALL insertChild("20070707-0707", "Simon Eldstrand", "2007-07-07");
+INSERT INTO barn(PNR, namn, födelseår) VALUES ("20070707-0707", "Simon Eldstrand", "2007-07-07");
+
+INSERT INTO inspelning(tid, beskrivning, kvalitet, filnamn, PNR, namn) VALUES("2022-09-12 10:25:00", "SimonFilm", 78, "Simon", "20070707-0707","Simon Eldstrand");
+
+INSERT INTO mindreSnälltBarn VALUES("20070707-0707", "Simon Eldstrand", 70, 32);
+
+INSERT INTO barnRelation(PNR1, namn1, PNR2, namn2, typAvRelation) VALUES ("20070707-0707", "Simon Eldstrand", "20090909-0909", "Anders Andersson", "Kusiner");
+
+SELECT * FROM barnRelation;
+
 CALL uppdateraBarnBeskrivning("20070707-0707", "Simon Eldstrand", "Detta är en ny beskrivning");
 CALL uppdateraBarnBeskrivning("20090909-0909", "Anders Andersson", "Detta är en uppdaterad beskrivning");
 SELECT * FROM barnBeskrivning;
+SELECT * FROM barnBeskrivningLOG;
+
+SELECT * FROM mindreSnälltBarn;
+SELECT * FROM snälltBarn;
+
+CALL tillMindreSnälltBarn("20090909-0909","Anders Andersson", 2, 1);
+
+SELECT * FROM barnLog;
+
+CALL tillSnälltBarn("20090909-0909","Anders Andersson");
+
+SELECT * FROM barnLog;
+SELECT * FROM barnetsInspelning;

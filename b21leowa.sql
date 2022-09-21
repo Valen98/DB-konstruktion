7fc2f2f179a2f3fd907c9ba92c1e4fe2ab44990a
Valen98 <leo.wahlandtcs@gmail.com>
2022-09-21 17:16:25 +0200

    Database complete i think

--- b21leowa.sql
+++ b21leowa.sql
@@ -3,8 +3,6 @@ CREATE DATABASE b21leowa;
 FLUSH PRIVILEGES;
 USE b21leowa;
 
--- Tables
-
 -- DEFAULT TABLE FOR BARN 
 CREATE TABLE barn(
 	PNR CHAR(14) NOT NULL,
@@ -13,7 +11,6 @@ CREATE TABLE barn(
     snällhetsSkala SMALLINT,
     PRIMARY KEY (PNR, namn)
 )ENGINE=INNODB;
-
 CREATE INDEX barnFödelseår ON barn(födelseår);
 
  CREATE TABLE barnRelation(
@@ -34,16 +31,16 @@ CREATE TABLE snällhetsKod (
 )ENGINE=INNODB;
 
 INSERT INTO snällhetsKod(ID, snällhet) VALUES (0, "Inte snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (10, "Inte särkilt snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (20, "Hyffsat snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (30, "Ganska snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (40, "Lagom snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (50, "Snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (60, "Rätt så snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (70, "Ganska mycket snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (80, "Bra mycket snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (90, "Riktigt snäll");
-INSERT INTO snällhetsKod(ID, snällhet) VALUES (100, "Super snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (1, "Inte särskilt snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (2, "Hyfsat snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (3, "Ganska snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (4, "Lagom snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (5, "Snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (6, "Rätt så snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (7, "Ganska mycket snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (8, "Bra mycket snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (9, "Riktigt snäll");
+INSERT INTO snällhetsKod(ID, snällhet) VALUES (10, "Super snäll");
 
 CREATE TABLE barnLog (
 	ID INTEGER NOT NULL AUTO_INCREMENT,
@@ -57,7 +54,7 @@ CREATE TABLE barnLog (
 )ENGINE=INNODB;
 
 -- INSERT DEFAULT CHILD FOR TESTING THE DB
-INSERT INTO barn(PNR, namn, födelseår) VALUES ("20090909-0909", "Anders Andersson", "2009-09-09");
+INSERT INTO barn(PNR, namn, födelseår, snällhetsSkala) VALUES ("20090909-0909", "Anders Andersson", "2009-09-09", 6);
 
 # Sysslor är antal sysslor som barnet har tilldelats och hur av dom sysslor hen har utfört. Därefter beräknas hjälpsamhet genom sysslor/utfördaSysslor
 -- TABLE FOR BARN INHERITANCE
@@ -88,7 +85,7 @@ INSERT INTO sysslorKod(ID, nivå) VALUES (8, "Rätt bra");
 INSERT INTO sysslorKod(ID, nivå) VALUES (9, "Riktigt bra");
 INSERT INTO sysslorKod(ID, nivå) VALUES (10, "Sjukt bra");
 
-INSERT INTO snälltBarn(PNR, namn, sysslor, utfördaSysslor, sysslorNivå) VALUES ("20090909-0909", "Anders Andersson", 12, 8,10);
+INSERT INTO snälltBarn(PNR, namn, sysslor, utfördaSysslor, sysslorNivå) VALUES ("20090909-0909", "Anders Andersson", 12, 12,10);
 
 -- TABLE FOR BARN INHERITANCE
 CREATE TABLE mindreSnälltBarn(
@@ -96,12 +93,15 @@ CREATE TABLE mindreSnälltBarn(
     namn VARCHAR(255) NOT NULL,
     nivå SMALLINT,
     leveransNummer CHAR(10),
+	sysslor SMALLINT,
+    utfördaSysslor SMALLINT,
+    sysslorNivå TINYINT,
     FOREIGN KEY (PNR, namn) REFERENCES barn(PNR, namn),
     PRIMARY KEY(PNR, namn)
 )ENGINE=INNODB;
 
 
--- Horizontell split from child and childText
+-- Horizsontell split from barn and barnBeskrivning
 CREATE TABLE barnBeskrivning (
     beskrivning TEXT,
 	PNR CHAR(14) NOT NULL,
@@ -123,10 +123,13 @@ CREATE TABLE inspelning(
 	tid DATETIME NOT NULL,
     beskrivning VARCHAR(11),
     kvalitet TINYINT,
-    filnamn VARCHAR(10),
+    filnamn VARCHAR(255),
     PNR CHAR(14) NOT NULL,
     namn VARCHAR(255) NOT NULL,
+    targetBarnPNR CHAR(14) NOT NULL,
+    targetBarnNamn VARCHAR(255) NOT NULL,
     FOREIGN KEY(PNR, namn) REFERENCES barn(PNR, namn),
+    FOREIGN KEY(targetBarnPNR, targetBarnNamn) REFERENCES barn(PNR, namn),
     PRIMARY KEY (tid, PNR, namn)
 )ENGINE=INNODB;
 
@@ -144,8 +147,8 @@ CREATE TABLE inspelningsText(
 
 CREATE TABLE önskelista(
 	årtal DATE NOT NULL,
-    medgiven TINYINT,
-    levererad TINYINT,
+    medgiven BIT,
+    levererad BIT,
     PNR CHAR(14) NOT NULL,
     namn VARCHAR(255) NOT NULL,
     FOREIGN KEY (PNR, namn) REFERENCES barn(PNR, namn),
@@ -169,39 +172,27 @@ INSERT INTO önskelistaBeskrivning(årtal, PNR, namn, beskrivning, kostnad) VALU
 
 #Förra årets önskelista
 INSERT INTO önskelista(årtal, medgiven, levererad, PNR, namn) VALUES ("2021-09-12", 0, 0, "20090909-0909", "Anders Andersson");
-INSERT INTO inspelning(tid, beskrivning, kvalitet, filnamn, PNR, namn) VALUES("2022-09-12 10:25:00", "AndersFilm", 78, "Anders", "20090909-0909","Anders Andersson");
+#INSERT INTO inspelning(tid, beskrivning, kvalitet, filnamn, PNR, namn) VALUES("2022-09-12 10:25:00", "AndersFilm", 78, "Anders", "20090909-0909","Anders Andersson");
 INSERT INTO barnBeskrivning(PNR, namn, beskrivning) VALUES("20090909-0909", "Anders Andersson" ,"Detta är en beskrivning");
 
 SELECT * FROM barnBeskrivning;
 
 -- Users
 CREATE USER IF NOT EXISTS 'barn'@'localhost' IDENTIFIED BY 'barn';
-CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin';
 CREATE USER IF NOT EXISTS 'tomten'@'localhost' IDENTIFIED BY 'tomten';
 
 GRANT SELECT, DELETE, UPDATE ON * TO 'tomten'@'localhost';
 
-SHOW GRANTS FOR 'admin'@'localhost';
-
-# Skapar en vy för att räkna ut hur mycket hjälpsamhet ett snälltbarn har. 0 är inga gjorda sysslor och 100 är alla sysslor gjorda och utifrån kvalitet på deras inspelningar.
-CREATE VIEW trovärdighet AS SELECT ((utfördaSysslor/sysslor)*sysslorNivå)*10*inspelning.kvalitet/100 AS trovärdighet, snälltBarn.PNR, snälltBarn.namn FROM snälltBarn, inspelning WHERE snälltBarn.PNR = inspelning.PNR;
-
-SELECT * FROM trovärdighet;
-
 #CREATE VIEW hjälpsamhet AS SELECT () AS hjälpsamhet, snälltBarn.PNR, snälltBarn.namn FROM snälltBarn
 
-# Skapar en vy som innehåller årets önskelistor.
-CREATE VIEW åretsÖnskelistor AS SELECT * FROM önskelista WHERE YEAR(årtal) = YEAR(NOW());
-
-SELECT * FROM åretsÖnskelistor;
-
 /*GIVES THE CHILDREN ACCESS TO SELECT, DELETE and UPDATE to barnetsÖnskelista and barnetsInspelning */
-GRANT INSERT, UPDATE, DELETE  ON önskelista TO 'barn'@'localhost';
-GRANT INSERT, UPDATE, DELETE ON inspelning  TO 'barn'@'localhost';
-
+GRANT SELECT, INSERT, UPDATE, DELETE  ON önskelista TO 'barn'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON inspelning  TO 'barn'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE  ON önskelistaBeskrivning TO 'barn'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE  ON inspelningsText TO 'barn'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE  ON barn TO 'barn'@'localhost';
 -- PUT ALL PROCEDURE AND TRIGGERS INSIDE THE DELIMITER
 DELIMITER //
-
 CREATE PROCEDURE uppdateraBarnBeskrivning(iPNR VARCHAR(14), iNamn VARCHAR(255), iBeskrivning TEXT)
 BEGIN 
     IF (SELECT PNR FROM barnBeskrivning WHERE PNR = iPNR AND namn = iNamn) THEN
@@ -220,86 +211,122 @@ END IF;
 END;
 
 #PROCEDURE Som flyttar ifall ett barn går från snäll till mindre snällt.
-CREATE PROCEDURE tillMindreSnälltBarn(iPNR VARCHAR(14), iNamn VARCHAR(255), iNivå INTEGER, iLeveransNummer CHAR(10)) 
+CREATE PROCEDURE tillMindreSnälltBarn(iPNR VARCHAR(14), iNamn VARCHAR(255), iNivå INTEGER, iLeveransNummer CHAR(10), iSysslor SMALLINT, iUtfördaSysslor SMALLINT, iSysslorNivå TINYINT) 
 BEGIN
 	IF (SELECT PNR FROM snälltBarn WHERE PNR = iPNR AND namn = iNamn) THEN 
 		DELETE FROM snälltBarn WHERE PNR = iPNR AND namn = iNamn;
-		INSERT INTO mindreSnälltBarn(PNR, namn, nivå, leveransNummer) VALUES (iPNR, iNamn, iNivå , iLeveransNummer);
+		INSERT INTO mindreSnälltBarn(PNR, namn, nivå, leveransNummer, sysslor, utfördaSysslor, sysslorNivå) VALUES (iPNR, iNamn, iNivå , iLeveransNummer, iSysslor, iUtfördaSysslor, iSysslorNivå);
 	ELSE 
 		IF(SELECT PNR FROM mindreSnälltBarn WHERE PNR != iPNR AND namn != iNamn) THEN
-			INSERT INTO mindreSnälltBarn(PNR, namn, nivå, leveransNummer) VALUES (iPNR, iNamn, iNivå , iLeveransNummer);
+			INSERT INTO mindreSnälltBarn(PNR, namn, nivå, leveransNummer) VALUES (iPNR, iNamn, iNivå , iLeveransNummer, iSysslor, iUtfördaSysslor, iSysslorNivå);
 		END IF;
 	END IF;
 END;
 
 #PROCEDURE Som flyttar ifall ett barn går från mindre snäll till snällt. Ifall 
-CREATE PROCEDURE tillSnälltBarn(iPNR VARCHAR(14), iNamn VARCHAR(255)) 
+CREATE PROCEDURE tillSnälltBarn(iPNR VARCHAR(14), iNamn VARCHAR(255), iSysslor SMALLINT, iUtfördaSysslor SMALLINT, iSysslorNivå TINYINT ) 
 BEGIN
 	#Kollar ifall barnet är med i mindreSnällt och ifall barnet är där tas barnet bort från mindre snällt tabellen.
 	IF (SELECT PNR FROM mindreSnälltBarn WHERE PNR = iPNR AND namn = iNamn) THEN 
 		DELETE FROM mindreSnälltBarn WHERE PNR = iPNR AND namn = iNamn;
-		INSERT INTO snälltBarn(PNR, namn) VALUES (iPNR, iNamn);
+		INSERT INTO snälltBarn(PNR, namn) VALUES (iPNR, iNamn, iSysslor, iUtfördaSysslor, iSysslorNivå);
 	ELSE 
 		IF(SELECT PNR FROM snälltBarn WHERE PNR != iPNR AND namn != iNamn) THEN
-			INSERT INTO snälltBarn(PNR, namn) VALUES (iPNR, iNamn);
+			INSERT INTO snälltBarn(PNR, namn) VALUES (iPNR, iNamn, iSysslor, iUtfördaSysslor, iSysslorNivå);
+		END IF;
+	END IF;
+END;
+
+CREATE PROCEDURE skapaInspelning(iTid DATETIME, iBeskrivning VARCHAR(11), iKvalitet TINYINT, iFilnamn VARCHAR(255), iPNR VARCHAR(14), iNamn VARCHAR(255), iTargetBarnPNR CHAR(14), iTargetBarnNamn VARCHAR(255))
+BEGIN 
+	#Kollar så barnet inte lägger in en inspelning på sig själv
+    IF(iPNR != iTargetBarnPNR AND iNamn != iTargetBarnNamn) THEN
+		#Kollar ifall barnet finns
+		IF(SELECT PNR from barn WHERE PNR = iTargetBarnPNR AND namn = iTargetBarnNamn) THEN
+			#Kollar så barnen känner varandra
+            IF(SELECT PNR1 FROM barnRelation WHERE PNR1 = iPNR AND PNR2 = iTargetBarnPNR) THEN
+				INSERT INTO inspelning(tid, beskrivning, kvalitet, filnamn, PNR, namn, targetBarnPNR, targetBarnNamn) VALUES (iTid, iBeskrivning, iKvalitet, iFilnamn, iPNR, iNamn, iTargetBarnPNR, iTargetBarnNamn);
+			ELSEIF (SELECT PNR1 FROM barnRelation WHERE PNR2 = iPNR AND PNR1 = iTargetBarnPNR) THEN
+				INSERT INTO inspelning(tid, beskrivning, kvalitet, filnamn, PNR, namn, targetBarnPNR, targetBarnNamn) VALUES (iTid, iBeskrivning, iKvalitet, iFilnamn, iPNR, iNamn, iTargetBarnPNR, iTargetBarnNamn);
+			END IF;
 		END IF;
 	END IF;
 END;
 
 #Log på insert
-CREATE TRIGGER tillSnälltBarnTrigger AFTER INSERT ON snälltBarn
+CREATE TRIGGER tillSnälltBarnLog AFTER INSERT ON snälltBarn
  FOR EACH ROW BEGIN
 	INSERT INTO barnLog(OPERATION, username, PNR, namn, tid, tabell) VALUES ("INS", USER(), new.PNR, new.namn, NOW(), "snälltBarn");
 END;
 
 #Log på insert
-CREATE TRIGGER tillMindreSnälltBarnTrigger AFTER INSERT ON mindreSnälltBarn
+CREATE TRIGGER tillMindreSnälltBarnLog AFTER INSERT ON mindreSnälltBarn
  FOR EACH ROW BEGIN
- INSERT INTO barnLog(OPERATION, username, PNR, namn, tid, tabell) VALUES ("INS", USER(), new.PNR, new.namn, NOW(), "mindreSnälltBarn");
+	INSERT INTO barnLog(OPERATION, username, PNR, namn, tid, tabell) VALUES ("INS", USER(), new.PNR, new.namn, NOW(), "mindreSnälltBarn");
+ END;
+ 
+ CREATE TRIGGER delSnälltBarnLog AFTER DELETE ON snälltBarn
+ FOR EACH ROW BEGIN
+	INSERT INTO barnLog(OPERATION, username, PNR, namn, tid, tabell) VALUES ("DEL", USER(), OLD.PNR, OLD.namn, NOW(), "snälltBarn");
+END;
+ 
+ CREATE TRIGGER delMindreSnälltBarnLog AFTER DELETE ON mindreSnälltBarn
+ FOR EACH ROW BEGIN
+	INSERT INTO barnLog(OPERATION, username, PNR, namn, tid, tabell) VALUES ("DEL", USER(), OLD.PNR, OLD.namn, NOW(), "mindreSnälltBarn");
  END;
  
  #Log på insert
- CREATE TRIGGER barnBeskrvningTriggerINS AFTER INSERT ON barnBeskrivning
+ CREATE TRIGGER barnBeskrvningLogINS AFTER INSERT ON barnBeskrivning
  FOR EACH ROW BEGIN 
 	  INSERT INTO barnBeskrivningLOG(OPERATION, PNR, namn, tid) VALUES ("INS", new.PNR, new.Namn, NOW()); 
 END;
 
 #Log på update
-CREATE TRIGGER barnBeskrivningTriggerUDP AFTER UPDATE ON barnBeskrivning
+CREATE TRIGGER barnBeskrivningLogUDP AFTER UPDATE ON barnBeskrivning
 FOR EACH ROW BEGIN
 	INSERT INTO barnBeskrivningLOG(OPERATION, PNR, namn, tid) VALUES ("UPD", new.PNR, new.Namn, NOW()); 
-    
+
 END//
 DELIMITER ;
 
 
-INSERT INTO barn(PNR, namn, födelseår) VALUES ("20070707-0707", "Simon Eldstrand", "2007-07-07");
+INSERT INTO barn(PNR, namn, födelseår, snällhetsSkala) VALUES ("20070707-0707", "Simon Eldstrand", "2007-07-07", 5);
 
-INSERT INTO inspelning(tid, beskrivning, kvalitet, filnamn, PNR, namn) VALUES("2022-09-12 10:25:00", "SimonFilm", 78, "Simon", "20070707-0707","Simon Eldstrand");
 
-INSERT INTO mindreSnälltBarn VALUES("20070707-0707", "Simon Eldstrand", 70, 32);
+SELECT * FROM inspelning;
+
+INSERT INTO snälltBarn VALUES("20070707-0707", "Simon Eldstrand", 10, 7, 5);
+CALL tillMindreSnälltBarn("20070707-0707", "Simon Eldstrand", 2, 1, 12, 8, 5);
 
 INSERT INTO barnRelation(PNR1, namn1, PNR2, namn2, typAvRelation) VALUES ("20070707-0707", "Simon Eldstrand", "20090909-0909", "Anders Andersson", "Kusiner");
 
-SELECT * FROM barnRelation;
 
 CALL uppdateraBarnBeskrivning("20070707-0707", "Simon Eldstrand", "Detta är en ny beskrivning");
 CALL uppdateraBarnBeskrivning("20090909-0909", "Anders Andersson", "Detta är en uppdaterad beskrivning");
 SELECT * FROM barnBeskrivning;
 SELECT * FROM barnBeskrivningLOG;
 
-SELECT * FROM mindreSnälltBarn;
-SELECT * FROM snälltBarn;
-
-CALL tillMindreSnälltBarn("20090909-0909","Anders Andersson", 2, 1);
-
-SELECT * FROM barnLog;
-
-CALL tillSnälltBarn("20090909-0909","Anders Andersson");
-
-SELECT * FROM barnLog;
-
-SELECT * FROM snällhetsKod;
+#CALL tillMindreSnälltBarn("20090909-0909","Anders Andersson", 2,1, 10, 9, 9);
 
 CALL skapaÖnskelista(NOW(),"20070707-0707", "Simon Eldstrand", "Fotboll vill jag ha", "2007-07-07");
-SELECT * FROM önskelista;
+SELECT * FROM barnRelation WHERE PNR1 = "20070707-0707" AND PNR2 = "20090909-0909";
+
+CALL skapaInspelning(NOW(), "AndersFilm",100, "Anders",  "20090909-0909", "Anders Andersson", "20070707-0707", "Simon Eldstrand");
+CALL skapaInspelning("2022-09-12 10:25:00", "SimonFilm", 78, "Simon", "20070707-0707", "Simon Eldstrand","20090909-0909", "Anders Andersson" );
+
+SELECT * FROM inspelning;
+
+# Skapar en vy för att räkna ut hur mycket hjälpsamhet ett snälltbarn har. 0 är inga gjorda sysslor och 100 är alla sysslor gjorda och utifrån kvalitet på deras inspelningar.
+CREATE VIEW trovärdighet AS SELECT ((utfördaSysslor/sysslor)*sysslorNivå)*inspelning.kvalitet/10 AS trovärdighet, snälltBarn.PNR, snälltBarn.namn FROM snälltBarn, inspelning WHERE snälltBarn.PNR = inspelning.PNR;
+
+# Skapar en vy för att räkna ut hur mycket hjälpsamhet ett snälltbarn har. 0 är inga gjorda sysslor och 100 är alla sysslor gjorda och utifrån kvalitet på deras inspelningar.
+CREATE VIEW mindreTrovärdighet AS SELECT ((utfördaSysslor/sysslor)*sysslorNivå)*(inspelning.kvalitet/10)-10 AS trovärdighet, mindreSnälltBarn.PNR, mindreSnälltBarn.namn FROM mindreSnälltBarn, inspelning WHERE mindreSnälltBarn.PNR = inspelning.PNR;
+
+SELECT * FROM trovärdighet;
+
+SELECT * FROM mindreTrovärdighet;
+
+# Skapar en vy som innehåller årets önskelistor.
+CREATE VIEW åretsÖnskelistor AS SELECT * FROM önskelista WHERE YEAR(årtal) = YEAR(NOW());
+
+SELECT * FROM åretsÖnskelistor;
